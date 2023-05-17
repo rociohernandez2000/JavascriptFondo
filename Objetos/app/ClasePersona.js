bfa7c6751dfa47f1f89c1a86667c5c370db35ef1
ferfcb99 <fer_fcb99@outlook.com>
2023-05-16 21:05:18 -0600

    Se complementa clase de ClasePersona

--- Objetos/app/ClasePersona.js
+++ Objetos/app/ClasePersona.js
@@ -1,10 +1,13 @@
 'use strict';
 
 class ClasePersona{
+    
     constructor(nombre, apellido, edad){
         this.nombre=nombre;
         this.apellido=apellido;
         this.edad=edad;
+        this.personas = [];
+        this.cantidadPersonas = 0;
     }
     getNombre(){
         return this.nombre;
@@ -25,8 +28,60 @@ class ClasePersona{
     setEdad(edad){
         this.edad=edad;
     }
+
+    pideCantidadPersonas(){
+        this.cantidadPersonas = prompt("¿Cuantas personas quieres registrar?");
+        console.log("Vas a registrar: " + this.cantidadPersonas);
+    }
+
+    /*
+    pideDatos(){
+        for (let i = 0; i < this.cantidadPersonas; i++) {
+            let personaActual = [];
+            console.log("Estas ingresando a la persona numero: " + i);
+            this.nombre = prompt("Ingresa el nombre: ");
+            this.apellido=prompt("Ingresa apellido: ");
+            this.edad=prompt("Ingresa edad: ");
+
+            personaActual.push(this.nombre);
+            personaActual.push(this.apellido);
+            personaActual.push(this.edad);
+
+            this.personas.push(personaActual);
+        }
+    }
+*/
+/*
+    muestraPersonas(){
+        this.personas.forEach(persona =>{
+            console.log(persona[0]);
+            console.log(persona[1]);
+            console.log(persona[2]);
+            console.log("------------")
+        })
+    }
+*/
+    pideDatosPorObjetos(){
+        for (let i = 0; i < this.cantidadPersonas; i++) {
+            
+            console.log("Estas ingresando a la persona numero: " + i);
+            const nombre = prompt("Ingresa el nombre: ");
+            const apellido=prompt("Ingresa apellido: ");
+            const edad=prompt("Ingresa edad: ");
+
+            let personaActual = new ClasePersona(nombre, apellido, edad);
+            this.personas.push(personaActual);
+        }
+        console.log(this.personas);
+    }
+
 }
 
-const mensajero=new ClasePersona("Rocio", "Hernandez",20);
 
-console.log(mensajero.getEdad())
+const mensajero = new ClasePersona("Rocio", "Hernandez",20);
+mensajero.nombre
+
+mensajero.pideCantidadPersonas()
+mensajero.pideDatosPorObjetos()
+
+
