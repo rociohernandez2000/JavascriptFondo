98ae65f9850eb107fb96d84c2fe3de5c826f3c6b
ferfcb99 <fer_fcb99@outlook.com>
2023-06-20 20:54:54 -0600

    Se agrega funcion que filtra los libros por titulo y descripcion

--- Objetos/app/FiltroLibros/js/app.js
+++ Objetos/app/FiltroLibros/js/app.js
@@ -4,14 +4,14 @@ console.log("esta enlazado")
 
 //variables
 const libros = [
-    [1, "Titulo1", "Descripcion 1", "../img/gato.png"],
-    [2, "Titulo2", "Descripcion 2", "../img/gato.png"],
-    [3, "Titulo3", "Descripcion 3", "../img/gato.png"],
-    [4, "Titulo4", "Descripcion 4", "../img/gato.png"],
-    [5, "Titulo5", "Descripcion 5", "../img/gato.png"],
-    [6, "Titulo6", "Descripcion 6", "../img/gato.png"],
-    [7, "Titulo7", "Descripcion 7", "../img/gato.png"],
-    [8, "Titulo8", "Descripcion 8", "../img/gato.png"],
+    [1, "La teoria del todo6", "Descripcion 1", "../img/gato.png"],
+    [2, "El monje que vendio su ferrari", "Descripcion 2", "../img/gato.png"],
+    [3, "Base de datos", "Descripcion 3", "../img/gato.png"],
+    [4, "Inteligencia artificial", "Descripcion 4", "../img/gato.png"],
+    [5, "Inteligencia artificial", "Descripcion 5", "../img/gato.png"],
+    [6, "EL monje que vendio su ferrari", "Descripcion 6", "../img/gato.png"],
+    [7, "Inteligencia artificial con datos", "Descripcion 7", "../img/gato.png"],
+    [8, "Policia moderna", "Descripcion 8", "../img/gato.png"],
 ];
 const campoTexto = document.querySelector('.entrada_texto');
 const campoBoton = document.querySelector('.entrada_boton');
@@ -19,15 +19,19 @@ const campoBoton = document.querySelector('.entrada_boton');
 /* console.log(campoTexto)
 console.log(campoTexto.required) */
 
+/*
 campoTexto.addEventListener("blur", function (evento) {
     console.log("Lo siguiente es resultado del evento blur")
     console.log(evento.target.value);
 });
+*/
 
 campoTexto.addEventListener("input", function (evento) {
-    console.log(evento)
+   //  console.log(evento.target.value)
+    const cadenaBusqueda = evento.target.value;
+    const librosFiltrados = filtraLibrosPorTituloYDescripcion(cadenaBusqueda, libros, 1, 2);
 
-})
+});
 
 campoBoton.addEventListener("click", function (evento) {
     console.log(evento);
@@ -55,10 +59,41 @@ function verificaExisteSubcadena(cadena, subcadena) {
     }
     return bandera;
 }
+/*
 console.log(verificaExisteSubcadena("Rocio", "Roc"));
 console.log(verificaExisteSubcadena("Rocio", "io"));
 console.log(verificaExisteSubcadena("Rocio", "a"));
-
-function filtralibroporTitulo(titulo, libros) {
-
+*/
+// true si esta, false sino.
+function filtraLibroPorTitulo(subcadena, libro, indiceTitulo) {
+    const tituloLibro = libro[indiceTitulo];
+    // console.log(verificaExisteSubcadena(tituloLibro, subcadena));
+    if(verificaExisteSubcadena(tituloLibro, subcadena)){
+        // console.log(`La subcadena ${subcadena} esta contenida en el titulo ${tituloLibro}`)
+        return true;
+    }
+    return false;
 }
+
+
+function filtraLibroPorDescripcion(subcadena, libro, indiceDescripcion){
+    const descripcionLibro = libro[indiceDescripcion];
+    // console.log(verificaExisteSubcadena(descripcionLibro, subcadena));
+    if(verificaExisteSubcadena(descripcionLibro, subcadena)){
+        // console.log(`La subcadena ${subcadena} esta contenida en el titulo ${descripcionLibro}`)
+        return true;
+    }
+    return false;
+}
+
+function filtraLibrosPorTituloYDescripcion(subcadena, libros, indiceTitulo, indiceDescripcion){
+    const librosFiltrados = libros.filter( libro => {
+        if(filtraLibroPorTitulo(subcadena, libro, indiceTitulo) || filtraLibroPorDescripcion(subcadena, libro, indiceDescripcion)){
+            return libro;
+        }
+    });
+    // console.log(librosFiltrados)
+    return librosFiltrados;
+}
+
+filtraLibrosPorTituloYDescripcion('6', libros, 1, 2);
